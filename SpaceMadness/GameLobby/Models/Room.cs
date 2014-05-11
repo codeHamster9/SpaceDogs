9f79ddc1104c0d6b05b95ceabe4b0dbf3ab21b3f
codeHamster9 <idansagi@gmail.com>
2014-05-11 16:15:01 +0300

    first commit

---
+++ SpaceMadness/GameLobby/Models/Room.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GameLobby.Models
+{
+    public class Room
+    {
+        public string RoomID { get; set; }
+
+        public int MaxNumPlayersInRoom { get; set; }
+
+        public int NumPlayersInRomm { get; set; }
+
+        public List<Player> players = new List<Player>();
+
+        public Room()
+        {
+            MaxNumPlayersInRoom = 2;
+            NumPlayersInRomm = 0;
+        }
+
+        public Room(int maxNumPlayer)
+        {
+            MaxNumPlayersInRoom = maxNumPlayer;
+            NumPlayersInRomm = 0;
+        }
+
+        public void AddPlayer(Player p)
+        {
+            players.Add(p);
+            NumPlayersInRomm = players.Count();
+        }
+
+        public void RemovePlayer(Player p)
+        {
+            players.Remove(p);
+            NumPlayersInRomm = players.Count();
+        }
+    }
+}
---
+++ SpaceMadness/SpaceWarz/Entities/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SpaceWarz.Entities
+{
+    public class Player
+    {
+        public string UserID { get; set; }
+        public string ConnectionID { get; set; }
+        public int Score { get; set; }
+    }
+}
